50385c1def4b58a576f701c5b15254f852a78a45
Fred Yin <41224888+yinfredyue@users.noreply.github.com>
2019-01-11 18:51:24 +0800

    Add files via upload

---
+++ machine-learning-ex7/ex7/computeCentroids.m
@@ -0,0 +1,46 @@
+function centroids = computeCentroids(X, idx, K)
+%COMPUTECENTROIDS returns the new centroids by computing the means of the 
+%data points assigned to each centroid.
+%   centroids = COMPUTECENTROIDS(X, idx, K) returns the new centroids by 
+%   computing the means of the data points assigned to each centroid. It is
+%   given a dataset X where each row is a single data point, a vector
+%   idx of centroid assignments (i.e. each entry in range [1..K]) for each
+%   example, and K, the number of centroids. You should return a matrix
+%   centroids, where each row of centroids is the mean of the data points
+%   assigned to it.
+%
+
+% Useful variables
+[m n] = size(X); 
+
+% You need to return the following variables correctly.
+centroids = zeros(K, n);
+
+
+% ====================== YOUR CODE HERE ======================
+% Instructions: Go over every centroid and compute mean of all points that
+%               belong to it. Concretely, the row vector centroids(i, :)
+%               should contain the mean of the data points assigned to
+%               centroid i.
+%
+% Note: You can use a for-loop over the centroids to compute this.
+%
+
+for k=1:K
+   _count = 0;
+   new_mu_k = zeros(1, size(X,2));
+   for i = 1:m
+     if idx(i) == k
+       _count += 1;
+       new_mu_k += X(i,:);
+     end
+   end
+   new_mu_k /= _count;
+   centroids(k,:) = new_mu_k;
+end
+
+% =============================================================
+
+
+end
+
